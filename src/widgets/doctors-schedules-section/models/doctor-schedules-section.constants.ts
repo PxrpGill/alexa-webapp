72573ce9d49480dc940076d8569d88ee64f6989b
PxrpGill <jhongil3007@gmail.com>
2026-07-11 12:28:32 +0300

    feat: добавить константу порядка дней недели

---
+++ src/widgets/doctors-schedules-section/models/doctor-schedules-section.constants.ts
@@ -0,0 +1,10 @@
+import type { DayOfWeek } from '../types/doctor-schedules-section.types';
+
+export const DAYS_OF_WEEK: { key: DayOfWeek; label: string }[] = [
+  { key: 'monday', label: 'Понедельник' },
+  { key: 'tuesday', label: 'Вторник' },
+  { key: 'wednesday', label: 'Среда' },
+  { key: 'thursday', label: 'Четверг' },
+  { key: 'friday', label: 'Пятница' },
+  { key: 'saturday', label: 'Суббота' },
+];
