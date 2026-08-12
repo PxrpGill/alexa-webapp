2a2bf4d5f9db25ce97d45fa2e8fe2d9f394e87b5
PxrpGill <jhongil3007@gmail.com>
2026-08-12 18:32:45 +0300

    feature: исправить нажатия на кнопки

--- src/widgets/solutions-section/ui/solution-card/index.tsx
+++ src/widgets/solutions-section/ui/solution-card/index.tsx
@@ -1,46 +1,48 @@
 /** biome-ignore-all lint/security/noDangerouslySetInnerHtml: intentional suppression */
 
-import Link from 'next/link';
+"use client";
 
-import ArrowSVG from '@/public/icons/arrow.svg';
-import Picture from '@/shared/ui/picture';
+import Link from "next/link";
 
-import type { SolutionCardProps } from '../../types/solutions-section.types';
-
-import css from './index.module.css';
+import ArrowSVG from "@/public/icons/arrow.svg";
+import { handleSectionScroll } from "@/shared/helpers/handle-section-scroll";
+import Picture from "@/shared/ui/picture";
+import type { SolutionCardProps } from "../../types/solutions-section.types";
+import css from "./index.module.css";
 
 export default function SolutionCard({
-    title,
-    description,
-    poster,
-    className,
-    href,
+	title,
+	description,
+	poster,
+	className,
+	href,
 }: SolutionCardProps) {
-    return (
-        <article className={`${css.root} ${className}`}>
-            <div className={css.content}>
-                {title && (
-                    <h5
-                        dangerouslySetInnerHTML={{ __html: title }}
-                        className={css.title}
-                    />
-                )}
-                {description && (
-                    <p
-                        dangerouslySetInnerHTML={{ __html: description }}
-                        className={css.description}
-                    />
-                )}
-            </div>
-            {poster && <Picture poster={poster} />}
-            <ArrowSVG className={css.arrow} />
-            {href && (
-                <Link
-                    href={href}
-                    className={css.link}
-                    aria-label="Оставить заявку"
-                />
-            )}
-        </article>
-    );
+	return (
+		<article className={`${css.root} ${className}`}>
+			<div className={css.content}>
+				{title && (
+					<h5
+						dangerouslySetInnerHTML={{ __html: title }}
+						className={css.title}
+					/>
+				)}
+				{description && (
+					<p
+						dangerouslySetInnerHTML={{ __html: description }}
+						className={css.description}
+					/>
+				)}
+			</div>
+			{poster && <Picture poster={poster} />}
+			<ArrowSVG className={css.arrow} />
+			{href && (
+				<Link
+					href={href}
+					className={css.link}
+					onClick={handleSectionScroll}
+					aria-label="Оставить заявку"
+				/>
+			)}
+		</article>
+	);
 }
