0c1764b158d0de460dcad875da38413783e66d47
PxrpGill <jhongil3007@gmail.com>
2026-08-12 14:53:38 +0300

    feature: добавить dumb-ui смены филиала

--- src/widgets/layout/ui/header/contact-part/index.tsx
+++ src/widgets/layout/ui/header/contact-part/index.tsx
@@ -1,51 +1,52 @@
-'use client';
+"use client";
 
-import MaxSVG from '@/public/icons/max.svg';
-import MenuSVG from '@/public/icons/menu-icon.svg';
-import PhoneSVG from '@/public/icons/phone.svg';
-import TelegramSVG from '@/public/icons/telegram.svg';
+import ChangeBranch from "@/features/change-branch";
+import MaxSVG from "@/public/icons/max.svg";
+import MenuSVG from "@/public/icons/menu-icon.svg";
+import PhoneSVG from "@/public/icons/phone.svg";
+import TelegramSVG from "@/public/icons/telegram.svg";
 import {
-    MAX,
-    MOBILE_PHONE,
-    MOBILE_PHONE_1,
-    TELEGRAM,
-} from '@/shared/config/global-constants.constats';
-import { useLayoutContext } from '@/shared/config/layout-context';
-
-import css from './index.module.css';
+	MAX,
+	MOBILE_PHONE,
+	MOBILE_PHONE_1,
+	TELEGRAM,
+} from "@/shared/config/global-constants.constats";
+import { useLayoutContext } from "@/shared/config/layout-context";
+import css from "./index.module.css";
 
 export default function ContactPart() {
-    const { toggleMenuOpen } = useLayoutContext();
+	const { toggleMenuOpen } = useLayoutContext();
 
-    return (
-        <div className={`${css.root}`.trim()}>
-            <a href={`tel:${MOBILE_PHONE}`} className={css.phone}>
-                {MOBILE_PHONE}
-            </a>
-            <a
-                className={css.telegram}
-                href={TELEGRAM}
-                aria-label="Перейти в Телеграм"
-            >
-                <TelegramSVG className={css.circleIcon} />
-            </a>
-            <a href={MAX} className={css.max} aria-label="Перейти в Max">
-                <MaxSVG className={css.circleIcon} />
-            </a>
-            <a
-                href={`tel:${MOBILE_PHONE_1}`}
-                className={css.anotherPhone}
-                aria-label="Позвонить по номеру телефона"
-            >
-                <PhoneSVG className={css.circleIcon} />
-            </a>
-            <button
-                className={css.menuButton}
-                type="button"
-                onClick={() => toggleMenuOpen(true)}
-            >
-                <MenuSVG className={css.menuIcon} />
-            </button>
-        </div>
-    );
+	return (
+		<div className={`${css.root}`.trim()}>
+			<ChangeBranch className={css.changeBranch} />
+			<a href={`tel:${MOBILE_PHONE}`} className={css.phone}>
+				{MOBILE_PHONE}
+			</a>
+			<a
+				className={css.telegram}
+				href={TELEGRAM}
+				aria-label="Перейти в Телеграм"
+			>
+				<TelegramSVG className={css.circleIcon} />
+			</a>
+			<a href={MAX} className={css.max} aria-label="Перейти в Max">
+				<MaxSVG className={css.circleIcon} />
+			</a>
+			<a
+				href={`tel:${MOBILE_PHONE_1}`}
+				className={css.anotherPhone}
+				aria-label="Позвонить по номеру телефона"
+			>
+				<PhoneSVG className={css.circleIcon} />
+			</a>
+			<button
+				className={css.menuButton}
+				type="button"
+				onClick={() => toggleMenuOpen(true)}
+			>
+				<MenuSVG className={css.menuIcon} />
+			</button>
+		</div>
+	);
 }
