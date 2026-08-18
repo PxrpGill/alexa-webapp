733021bee456d94c41d8c4f56ddde1a645d84233
PxrpGill <jhongil3007@gmail.com>
2026-08-18 15:03:43 +0300

    feature: сделать страницу новости ISR

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,20 @@
 import { notFound } from "next/navigation";
+import { getAllNews } from "@/entities/news/api/get-all-news";
 import { getSingleNews } from "@/entities/news/api/get-single-news";
 import SingleBlogPage from "@/views/single-blog-page";
 
 type SingleBlogPageParams = {
 	params: Promise<{ slug: string }>;
+};
+
+export const revalidate = 60;
+
+export const generateStaticParams = async () => {
+	const response = await getAllNews();
+
+	if (!response?.items?.length) return [];
+
+	return response.items.map((news) => ({ slug: news.slug }));
 };
 
 export default async function SingleBlog({ params }: SingleBlogPageParams) {
