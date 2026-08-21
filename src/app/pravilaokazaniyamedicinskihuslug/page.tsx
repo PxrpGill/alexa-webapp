f82ebfe4b1ab76669555f15da189a6a9ac42ae84
PxrpGill <jhongil3007@gmail.com>
2026-08-21 15:52:03 +0300

    feature: добавить страницу Правила оказания медицинских услуг

---
+++ src/app/pravilaokazaniyamedicinskihuslug/page.tsx
@@ -0,0 +1,7 @@
+import RulesForTheProvisionOfMedicalServicesPage from "@/views/rules-for-the-provision-of-medical-services-page";
+
+export const revalidate = 60;
+
+export default function RulesForTheProvisionOfMedicalServices() {
+	return <RulesForTheProvisionOfMedicalServicesPage />;
+}
