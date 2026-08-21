ff70fae9b43e7f42bd8bf6fa45e2fc93efec5fd2
PxrpGill <jhongil3007@gmail.com>
2026-08-21 14:25:15 +0300

    feature: добавить страницу Правила внесения оплаты за медицинские услуги

---
+++ src/views/rules-for-payment-for-medical-services/index.tsx
@@ -0,0 +1,25 @@
+/** biome-ignore-all lint/suspicious/noArrayIndexKey: <explanation> */
+
+import { YANDEX_MAP_INFO_CARD } from "@/shared/config/global-constants.constants";
+import DocumentBlock from "@/shared/ui/document-block";
+import DocumentHero from "@/widgets/document-hero";
+import YandexMap from "@/widgets/yandex-map";
+import css from "./index.module.css";
+import {
+	DOCUMENT_BLOCKS,
+	DOCUMENT_HERO_MOCK,
+} from "./models/rules-for-payment-for-medical-services.constants";
+
+export default function RulesforPaymentforMedicalServicesPage() {
+	return (
+		<main className={css.root}>
+			<DocumentHero {...DOCUMENT_HERO_MOCK} />
+			<div className={`${css.content} container`}>
+				{DOCUMENT_BLOCKS.map((documentBlock, index) => (
+					<DocumentBlock {...documentBlock} key={index} />
+				))}
+			</div>
+			<YandexMap infoCard={YANDEX_MAP_INFO_CARD} className={css.yandexMap} />
+		</main>
+	);
+}
